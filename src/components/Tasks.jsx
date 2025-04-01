86a02c7c8f90e0328d79e9dde0e224ba94839358
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-01 19:29:14 +0300

    Add animations on task delete with motion. Add scroll in description and task-list so that the entire Tasks has no scroll

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Tasks({ selectedProject, onDeleteSelectedProject, onAddNewTaskToSelectedProject, onDeleteTask }) {
 
@@ -17,7 +18,8 @@ export default function Tasks({ selectedProject, onDeleteSelectedProject, onAddN
 
     return (
         <div id="tasks" className="flex-auto">
-            <div className="flex flex-col p-10">
+            <div className="flex flex-col p-10 h-screen">
+
                 <div id="task-header" className="flex place-content-between items-center mt-4">
                     <h1 className="flex-auto font-bold text-3xl text-stone-700">{selectedProject.projectTitle}</h1>
                     <button
@@ -25,10 +27,12 @@ export default function Tasks({ selectedProject, onDeleteSelectedProject, onAddN
                         onClick={onDeleteSelectedProject}
                     >Delete</button>
                 </div>
+
                 <h2 className="text-slate-500 mt-2">{formattedDate}</h2>
-                <p className="mt-4">{selectedProject.projectDescription}</p>
+                <p className="mt-4 h-[25vh] overflow-y-auto">{selectedProject.projectDescription}</p>
                 <hr className="border-2 rounded-lg mt-4" />
                 <h1 className="font-bold text-2xl mt-4 text-stone-700">Tasks</h1>
+
                 <div id="tasks-controller">
                     <div id="task-add">
                         <input
@@ -42,29 +46,38 @@ export default function Tasks({ selectedProject, onDeleteSelectedProject, onAddN
                             onClick={addTaskToSelectedProject}
                         >+ Add Task</button>
                     </div>
+                </div>
+
+                <div id="task-list" className="h-[70vh] overflow-y-auto">
                     {selectedProject.projectTasks.length < 1 && <h3>This project does not have any tasks yet.</h3>}
                     <ul>
-                        {/* TODO ANBOL add scroll bar if there are a lot of tasks */}
-                        {
-                            selectedProject.projectTasks.map((task, index) => {
-                                return (
-                                    <li
-                                        key={index}
-                                        className="bg-stone-100 p-3 my-3 w-auto border-2 border-transparent rounded-md hover:border-stone-200">
-                                        <div className="flex place-content-between">
-                                            <label>
-                                                {task.taskTitle}
-                                            </label>
-                                            <button
-                                                onClick={() => onDeleteTask(task.taskId)}
-                                                className="mr-10 hover:text-red-600"
-                                            >Clear</button>
-                                        </div>
-                                    </li>);
-                            })
-                        }
+                        {/* Prevents animation on first load */}
+                        <AnimatePresence initial={false}>
+                            {
+                                selectedProject.projectTasks.map((task, index) => {
+                                    return (
+                                        <motion.li
+                                            key={index}
+                                            layout // Smoothly animates position of remaining items
+                                            initial={{ opacity: 0, y: -10 }} // Start slightly above
+                                            animate={{ opacity: 1, y: 0 }} // Fade in + move to normal
+                                            exit={{ opacity: 0, clipPath: "inset(0% 0% 0% 100%)" }} // Prevents shifting
+                                            transition={{ duration: 0.2 }}
+                                            className="bg-stone-100 p-3 my-3 w-auto border-2 border-transparent rounded-md hover:border-stone-200">
+                                            <div className="flex place-content-between">
+                                                <label>
+                                                    {task.taskTitle}
+                                                </label>
+                                                <button
+                                                    className="mr-10 hover:text-red-600"
+                                                    onClick={() => onDeleteTask(task.taskId)}
+                                                >Clear</button>
+                                            </div>
+                                        </motion.li>);
+                                })
+                            }
+                        </AnimatePresence>
                     </ul>
-
                 </div>
             </div>
         </div >
